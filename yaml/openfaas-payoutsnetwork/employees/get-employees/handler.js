ef4bc0f6e4d53666a4a81f4f4e7ef529686567fc
jpribyl <pribylsnbits@gmail.com>
2019-09-19 23:49:39 -0600

    added dbconfig

--- yaml/openfaas-payoutsnetwork/employees/get-employees/handler.js
+++ yaml/openfaas-payoutsnetwork/employees/get-employees/handler.js
@@ -1,10 +1,15 @@
 "use strict";
+const fs require('fs')
+const mysqlHostFile = '/var/openfaas/secrets/mysql-host'
+const encoding = 'utf-8'
 
 module.exports = (event, context) => {
   let err;
+  const host = fs.readFileSync(mysqlHostFile, encoding)
   const result = {
-    status: "You said: " + JSON.stringify(event.body),
-    mysqlHost: process.env["mysql-host"]
+    mysqlHost: host,
+    status: "You said: " + host,
+    more: "this is another key value pair",
   };
 
   context.status(200).succeed(result);
