f32d7ded59a3d478ad82b37be2202fb6c15e1d52
jpribyl <pribylsnbits@gmail.com>
2019-09-20 00:28:02 -0600

    added async

--- yaml/openfaas-payoutsnetwork/employees/get-employees/handler.js
+++ yaml/openfaas-payoutsnetwork/employees/get-employees/handler.js
@@ -1,13 +1,16 @@
 "use strict";
-const knex = require('../../configs/dbConfig.js')
-module.exports = (event, context) => {
+const knex = require("../../configs/dbConfig.js");
+module.exports = async (event, context) => {
   let err;
   let r;
+  const { page, perPage, sort, sortDirection } = {
+    ...JSON.parse(event.body)
+  };
   r = await knex
-      .select('employees.*', 'states.abbreviation as state_abbreviation')
-      .from('employees')
-      .leftJoin('states', 'employees.state_id', 'states.id')
-      .paginate(page, perPage);
+    .select("employees.*", "states.abbreviation as state_abbreviation")
+    .from("employees")
+    .leftJoin("states", "employees.state_id", "states.id")
+    .paginate(page, perPage);
   const result = {
     status: "success"
   };
